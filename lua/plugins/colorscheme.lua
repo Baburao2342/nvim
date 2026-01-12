f5c61d7f37df5d9f7e9aa0f3e8952c71a40dc92d
Parijat <parijatbasak06@gmail.com>
2026-01-12 10:33:51 +0530

    Back to Rose Pine

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,21 +6,11 @@ return {
     --         vim.cmd("colorscheme kanagawa")
     --     end
     -- },
-    -- {
-    --     "rose-pine/neovim",
-    --     name = "rose-pine",
-    --     config = function()
-    --         vim.cmd("colorscheme rose-pine")
-    --     end
-    -- },
     {
-        "sainnhe/everforest",
-        name = "everforest",
-        config = function ()
-            vim.o.background = 'dark' 
-            vim.g.everforest_background = "hard"
-            vim.cmd("colorscheme everforest")
-            vim.g.everforest_enable_italic = 1
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            vim.cmd("colorscheme rose-pine")
         end
-    }
+    },
 }
