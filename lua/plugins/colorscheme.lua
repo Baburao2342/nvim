162cc412c4d3b063df9daac07c871a11a1375705
Parijat <parijatbasak06@gmail.com>
2026-01-12 10:30:29 +0530

    Everforst

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,19 +1,26 @@
 return {
-    {  
-        "rebelot/kanagawa.nvim",
-        name = "kanagawa",
-        config = function()
-            vim.cmd("colorscheme kanagawa")
-        end
-    },
+    -- {  
+    --     "rebelot/kanagawa.nvim",
+    --     name = "kanagawa",
+    --     config = function()
+    --         vim.cmd("colorscheme kanagawa")
+    --     end
+    -- },
+    -- {
+    --     "rose-pine/neovim",
+    --     name = "rose-pine",
+    --     config = function()
+    --         vim.cmd("colorscheme rose-pine")
+    --     end
+    -- },
     {
-        {
-            "xero/miasma.nvim",
-            lazy = false,
-            priority = 1000,
-            config = function()
-                -- vim.cmd("colorscheme miasma")
-            end,
-        }
-    },
+        "sainnhe/everforest",
+        name = "everforest",
+        config = function ()
+            vim.o.background = 'dark' 
+            vim.g.everforest_background = "hard"
+            vim.cmd("colorscheme everforest")
+            vim.g.everforest_enable_italic = 1
+        end
+    }
 }
