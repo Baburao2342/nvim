5ceff59aca761b8e7808a7e85825a1a8ab40fb6c
Parijat <parijatbasak06@gmail.com>
2026-07-06 01:39:43 +0530

    Gruvbox

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,23 +1,24 @@
 return {
-  {
-    'folke/tokyonight.nvim',
-    priority = 1000,
-    ---@diagnostic disable-next-line: missing-fields
-    config = function()
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = true },
-        },
-      }
-      vim.cmd.colorscheme 'tokyonight-night'
-    end,
-  },
-
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {
-      options = { theme = 'auto' },
+    {
+        'sainnhe/gruvbox-material',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            -- Optionally configure and load the colorscheme
+            -- directly inside the plugin declaration.
+            vim.g.gruvbox_material_enable_italic = true
+            vim.g.gruvbox_material_enable_bold = true
+            vim.g.gruvbox_material_transparent_background = 2
+            vim.g.gruvbox_material_foreground = 'mix'
+            vim.cmd.colorscheme 'gruvbox-material'
+        end,
     },
-  },
+
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        opts = {
+            options = { theme = 'auto' },
+        },
+    },
 }
