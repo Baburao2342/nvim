c1d7df918a531ed27973630b49a7916f79514218
Parijat <parijatbasak06@gmail.com>
2026-07-25 17:39:59 +0530

    nah

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,12 +1,14 @@
 return {
-  'lewis6991/gitsigns.nvim',
-  opts = {
-    signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
+    'lewis6991/gitsigns.nvim',
+    opts = {
+        signs = {
+            add = { text = '+' },
+            change = { text = '~' },
+            delete = { text = '_' },
+            topdelete = { text = '‾' },
+            changedelete = { text = '~' },
+        },
+        current_line_blame = true,
+        current_line_blame_opts = { delay = 300 },
     },
-  },
 }
