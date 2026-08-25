57658d8461cc3c2d1289b28a8bf431224348a540
Parijat <parijatbasak06@gmail.com>
2026-08-25 21:27:34 +0530

    On Arch Linux with WSL :(

--- lua/plugins/colorschemes-mine.lua
+++ lua/plugins/colorschemes-mine.lua
@@ -1,4 +1,16 @@
 return {
-  "xiyaowong/transparent.nvim",
-  opts = {},
+  {
+    "xiyaowong/transparent.nvim",
+    opts = {},
+  },
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000,
+    config = function()
+      require("onedark").setup({
+        style = "dark",
+      })
+      require("onedark").load()
+    end,
+  },
 }
